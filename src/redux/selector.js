ba6cd9e1ab7acbb37c79c8d81d2982d28c68f12c
oleksii-zviezdin <oleksii.zviezdin@gmail.com>
2023-06-14 23:56:25 +0200

    optimalization

--- src/redux/selector.js
+++ src/redux/selector.js
@@ -2,13 +2,12 @@ export const getContacts = state => state.contacts.contacts;
 export const getFilter = state => state.contacts.filter;
 
 export const getFilteredContacts = (filter, contacts) => {
-  if (!filter) {
-    return contacts;
-  }
+  if (!filter) return contacts;
+
   const normalizedFilter = filter.toLowerCase();
-  if (contacts.length !== 0) {
-    return contacts.filter(contact =>
+
+  if (contacts.length !== 0)
+    contacts.filter(contact =>
       contact.inputName.toLowerCase().includes(normalizedFilter)
     );
-  }
 };
